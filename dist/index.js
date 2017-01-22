b83967b3adca17458f753974011c0830d1d8015e
Adam Recvlohe <adam.recvlohe@gmail.com>
2017-01-21 19:59:04 -0500

    Update styles

--- dist/index.js
+++ dist/index.js
@@ -13,6 +13,7 @@ var temp = makeTempDiv(createElement('div'))
 var makeFButton = R.compose(
   setListenter('click', function() { setTempText2F(STATE) }),
   setText('F'),
+  setClassName('button'),
   setId('fahrenheit')
 )
 var fButton = makeFButton(createElement('div'))
@@ -23,9 +24,15 @@ var fButton = makeFButton(createElement('div'))
 var makeCButton = R.compose(
   setListenter('click', function() { setTempText2C(STATE) }),
   setText('C'),
+  setClassName('button'),
   setId('celsius')
 )
 var cButton = makeCButton(createElement('div'))
+
+var makeButtonBox = R.compose(
+  setId('button-box')
+)
+var buttonBox = makeButtonBox(createElement('div'))
 
 // FUNCTIONS
 /**
@@ -36,7 +43,7 @@ var setTempText2F = setTempText('F')
 var setTempText2C = setTempText('C')
 
 var setAppText = setTextContent(app)
-var setAppClass = setClassName(app)
+var setAppErrorClass = setClassName('error')
 
 // AJAX
 /**
@@ -47,16 +54,19 @@ function onSuccess(position) {
   var lat = position.coords.latitude
   var lon = position.coords.longitude
   var url = API + lat + ',' + lon + '?exclude=minutely,hourly,daily,alerts,flags&lang=' + LANGUAGE + '&units=auto'
-
+  
   $.ajax({
     url: url,
     jsonp: 'callback',
     dataType: 'jsonp',
     success: function(response) {
       var temperature = response.currently.temperature
-      var renderUnderApp = appendElements(app)
+      var contents = appendElements(app)
+      var buttons = appendElements(buttonBox)
       setGlobalState(STATE, { fahrenheit: temperature, celsius: (temperature - 32) * (5/9) }, setTempText2F)
-      renderUnderApp([temp, fButton, cButton])
+      buttons([fButton, cButton])
+      contents([temp, buttonBox])
+
     }
   })
 }
@@ -66,11 +76,11 @@ function onSuccess(position) {
 * @param {Object} - The error object passed back from navigator
 */
 function onError(error) {
-  setAppClass('error')
+  setAppErrorClass(app)
   switch(error.code) {
     case error.PERMISSION_DENIED:
-     setAppText('User denied the request for Geolocation.')
-     break;
+      setAppText('User denied the request for Geolocation.')
+      break;
     case error.POSITION_UNAVAILABLE:
       setAppText('Location information is unavailable.')
       break;
