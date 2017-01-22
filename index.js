13a545415040a2051bc97547e4e2b2f0e34c34b7
Adam Recvlohe <adam.recvlohe@gmail.com>
2017-01-21 20:29:20 -0500

    Add namespacing to app

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/** @namespace */
+var app = {}
 /**
 * Create the div for where the temp will be displayed
 */
@@ -10,7 +12,7 @@ var temp = makeTempDiv(createElement('div'))
 * Create a button that when the user clicks displays the temperature in fahrenheit
 */
 var makeFButton = R.compose(
-  helpers.setListenter('click', function() { setTempText2F(STATE) }),
+  helpers.setListenter('click', function() { app.setTempText2F(STATE) }),
   helpers.setText('F'),
   helpers.setClassName('button'),
   helpers.setId('fahrenheit')
@@ -21,7 +23,7 @@ var fButton = makeFButton(createElement('div'))
 * Create a button that when the user clicks displays the temperature in celsius
 */
 var makeCButton = R.compose(
-  helpers.setListenter('click', function() { setTempText2C(STATE) }),
+  helpers.setListenter('click', function() { app.setTempText2C(STATE) }),
   helpers.setText('C'),
   helpers.setClassName('button'),
   helpers.setId('celsius')
@@ -35,16 +37,32 @@ var buttonBox = makeButtonBox(createElement('div'))
 
 /**
 * Create a function that resets the temp value
+* @memberof app
 */
 var setTempText = helpers.setTextContentTemp(temp)
-var setTempText2F = setTempText('F')
-var setTempText2C = setTempText('C')
+app.setTempText = setTempText
 
+/**
+* @memberof app
+*/
+var setTempText2F = app.setTempText('F')
+app.setTempText2F = setTempText2F
+/**
+* @memberof app
+*/
+var setTempText2C = app.setTempText('C')
+app.setTempText2C = setTempText2C
+
+/**
+* @memberof app
+*/
 var setAppText = helpers.setTextContent(global.app)
+app.setAppText = setAppText
 var setAppErrorClass = helpers.setClassName('error')
 
 /**
 * Handle the geolocation if it returns the users position
+* @memberof app
 * @param {Object} position - The coordinates of the user passed back from navigator
 */
 function onSuccess(position) {
@@ -60,43 +78,48 @@ function onSuccess(position) {
       var temperature = response.currently.temperature
       var contents = appendElements(global.app)
       var buttons = appendElements(buttonBox)
-      setGlobalState(STATE, { fahrenheit: temperature, celsius: (temperature - 32) * (5/9) }, setTempText2F)
+      setGlobalState(STATE, { fahrenheit: temperature, celsius: (temperature - 32) * (5/9) }, app.setTempText2F)
       buttons([fButton, cButton])
       contents([temp, buttonBox])
 
     }
   })
 }
+app.onSuccess = onSuccess
 
 /**
 * Handle geolocation if for some reason it fails
+* @memberof app
 * @param {Object} - The error object passed back from navigator
 */
 function onError(error) {
   setAppErrorClass(global.app)
   switch(error.code) {
     case error.PERMISSION_DENIED:
-      setAppText('User denied the request for Geolocation.')
+      app.setAppText('User denied the request for Geolocation.')
       break;
     case error.POSITION_UNAVAILABLE:
-      setAppText('Location information is unavailable.')
+      app.setAppText('Location information is unavailable.')
       break;
     case error.TIMEOUT:
-      setAppText('The request to get user location timed out.')
+      app.setAppText('The request to get user location timed out.')
       break;
     case error.UNKNOWN_ERROR:
-      setAppText('An unknown error occurred.')
+      app.setAppText('An unknown error occurred.')
       break;
    }
 }
+app.onError = onError
 
 /**
 * Run the program
+* @memberof app
 */
 function main() {
   if (global.NAV.geolocation) {
-    global.NAV.geolocation.getCurrentPosition(onSuccess, onError)
+    global.NAV.geolocation.getCurrentPosition(app.onSuccess, app.onError)
   }
 }
+app.main = main
 
-main()
+app.main()
